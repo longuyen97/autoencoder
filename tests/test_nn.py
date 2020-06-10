c011353ac5e1577c98b924fe388cfae6a82cf3c7
Long Nguyen <long.nguyen2306@protonmail.com>
2020-06-10 16:34:39 +0200

    yeah

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -38,6 +38,7 @@ class TestNN(unittest.TestCase):
         print(X.shape, Y.shape)
         net = NeuralNetwork([X.shape[0], 256, Y.shape[0]], Relu(), Softmax(), CategoricalCrossEntropy())
         last_loss = net.train(X, Y)
+        last_acc = 0
         for i in range(100):
             loss = net.train(X, Y)
             assert (loss < last_loss)
@@ -45,16 +46,21 @@ class TestNN(unittest.TestCase):
             prediction = np.argmax(net.predict(X), axis=0)
             ground_truth = np.argmax(Y, axis=0)
             acc = accuracy_score(prediction, ground_truth)
+            self.assertTrue(acc > last_acc)
+            last_acc = acc
             print(f"Epoch {i} Loss {last_loss.item()} Accuracy {acc}")
 
     def test_binary(self):
         X, Y = generate_binary_data()
         print(X.shape, Y.shape)
         net = NeuralNetwork([X.shape[0], 256, 1], Relu(), Sigmoid(), BinaryCrossEntropy())
+        last_acc = 0
         for i in range(100):
             loss = net.train(X, Y)
             prediction = np.round(net.predict(X))
             acc = accuracy_score(Y[0], prediction[0])
+            self.assertTrue(acc > last_acc)
+            last_acc = acc
             print(f"Epoch {i} Loss {loss.item()} Accuracy {acc}")
 
 
